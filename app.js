124f531a7bb0bcd0a12e7fe8fa3f6e22979ae9d0
NouraNou <oneway.33@hotmail.com>
2023-08-12 18:55:29 +0300

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const cors = require("cors");
 const express = require("express");
 const config = require("./config/keys");
 const authRoutes = require("./api/Auth/auth.routes");
+const bloodRequestRoutes = require("./api/BloodRequest/bloodRequest.routes");
 const recipientReqRoutes = require("./api/RecipientRequest/rr.routes");
 const paciRoutes = require("./api/Paci/paci.routes");
 const notFound = require("./middlewares/notFoundHandler");
@@ -32,6 +33,7 @@ app.use("/media", express.static(path.join(__dirname, "media")));
 app.use("/api/auth", authRoutes);
 app.use("/api/recipient", recipientReqRoutes);
 app.use("/api/paci", paciRoutes);
+app.use("/api/bloodRequest", bloodRequestRoutes);
 
 //errorhandlers:
 app.use(notFound);
